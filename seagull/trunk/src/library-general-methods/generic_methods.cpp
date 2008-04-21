0aaaf77807e52e104bad95283a001c474ca7a265
vidhya_kannan <vidhya_kannan@9920205e-2714-0410-b6de-9c83873d791f>
2008-04-21 12:18:10 +0000

    git-svn-id: svn://svn.code.sf.net/p/gull/code@345 9920205e-2714-0410-b6de-9c83873d791f

--- seagull/trunk/src/library-general-methods/generic_methods.cpp
+++ seagull/trunk/src/library-general-methods/generic_methods.cpp
@@ -24,11 +24,77 @@
 #include "Utils.hpp"
 #include <time.h>
 
+
+char* external_find_text_value (char *P_buf, char *P_field) {
+
+  char *L_value = NULL ;
+
+  regex_t    L_reg_expr ;
+  int        L_status ;
+  char       L_buffer[100];
+  regmatch_t L_pmatch[3] ;
+  size_t     L_size = 0 ;
+
+  string_t   L_string = "" ;
+
+  L_string  = "([[:blank:]]*" ;
+  L_string += P_field ;
+  L_string += "[[:blank:]]*=[[:blank:]]*)([^;]+)";
+
+  L_status = regcomp (&L_reg_expr,
+                      L_string.c_str(),
+                      REG_EXTENDED) ;
+
+  if (L_status != 0) {
+    regerror(L_status, &L_reg_expr, L_buffer, 100);
+    regfree (&L_reg_expr) ;
+  } else {
+
+    L_status = regexec (&L_reg_expr, P_buf, 3, L_pmatch, 0) ;
+    regfree (&L_reg_expr) ;
+    if (L_status == 0) {
+      L_size = L_pmatch[2].rm_eo - L_pmatch[2].rm_so ;
+      ALLOC_TABLE(L_value, char*, sizeof(char), L_size+1);
+      memcpy(L_value, &(P_buf[L_pmatch[2].rm_so]), L_size);
+      L_value[L_size]='\0' ;
+    }
+  }
+  return (L_value);
+}
+
+typedef struct args_string {
+  char * m_startoffset;
+} T_ArgsStr, *T_pArgsStr ;
+
+static const T_ArgsStr Args_Str_init = {
+  "0"
+} ;
+
+
+int args_analysis (T_pValueData  P_args, T_pArgsStr P_result) {
+
+  int             L_ret = 0 ;
+
+  *P_result = Args_Str_init ;
+  P_result->m_startoffset = external_find_text_value((char*)P_args->m_value.m_val_binary.m_value,
+                                             (char*)"startoffset")  ;
+  if (P_result->m_startoffset == NULL)
+  P_result->m_startoffset = "0";
+  return (L_ret);
+}
+
 int sys_time_ms (T_pValueData  P_msgPart,
                    T_pValueData  P_args,
                    T_pValueData  P_result) {
   int             L_ret    = 0    ;
+
+  T_ArgsStr L_args;
+
+  int l_ret = 0;
+
+  l_ret = args_analysis (P_args, &L_args);
   P_result->m_type = E_TYPE_NUMBER ;
-  P_result->m_value.m_val_number = time(NULL);
+  P_result->m_value.m_val_number = time(NULL) + atoi(L_args.m_startoffset);
   return (L_ret);
 }
+
