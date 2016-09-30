codeghar/Seagull

commit 2496ef5f7bafa11f6ccfc24d7169bb1cf27b7982

memory leak fix
git-svn-id: svn://svn.code.sf.net/p/gull/code@412 9920205e-2714-0410-b6de-9c83873d791f

seagull/trunk/src/generator-common/ExternalMethodDef.cpp
@@ -21,6 +21,7 @@
 #include "TextUtils.hpp"
 #include "GeneratorError.h"
 #include "GeneratorTrace.hpp"
+#include "Utils.hpp"
 #include "dlfcn_t.hpp"
 
 T_ExternalMethod create_external_method (char *P_args) {
@@ -37,6 +38,7 @@ T_ExternalMethod create_external_method (char *P_args) {
 	      "no name for the library for the filter (lib=...)");
   } else {
     L_library_handle = dlopen(L_lib_name, RTLD_LAZY);
+    FREE_TABLE(L_lib_name);
     if (L_library_handle == NULL) {
       GEN_ERROR(E_GEN_FATAL_ERROR, 
                 "Unable to open library file [" 
@@ -52,6 +54,7 @@ T_ExternalMethod create_external_method (char *P_args) {
   } else {
     if (L_library_handle) { 
       L_function = dlsym(L_library_handle, L_fun_name);
+      FREE_TABLE(L_fun_name);
       if (L_function == NULL) {
         GEN_ERROR(E_GEN_FATAL_ERROR, "Error [" << dlerror() << "]");
       } else {

commit 557c8921da8a67e22d59de83c2a32753019119aa

fix truncated error messages "Unable to open library file"
fix issues introduced by:
2496ef5 marc_k on 21.04.2011 at 12:48
memory leak fix

9514686 marc_k on 21.04.2011 at 12:47
memory leak fix

seagull/trunk/src/generator-common/ExternalMethodDef.cpp
@@ -38,13 +38,13 @@ T_ExternalMethod create_external_method (char *P_args) {
 	      "no name for the library for the filter (lib=...)");
   } else {
     L_library_handle = dlopen(L_lib_name, RTLD_LAZY);
-    FREE_TABLE(L_lib_name);
     if (L_library_handle == NULL) {
       GEN_ERROR(E_GEN_FATAL_ERROR, 
                 "Unable to open library file [" 
                 << L_lib_name
                 << "] error [" << dlerror() << "]");
     }
+    FREE_TABLE(L_lib_name);
   }
 
   L_fun_name = find_text_value(P_args,(char*)"function") ;

seagull/trunk/src/protocol-text/C_ProtocolText.cpp
@@ -195,7 +195,6 @@ int C_ProtocolText::set_filter_method (char *P_filter_method) {
     L_ret = -1;
   } else {
     L_library_handle = dlopen(L_lib_name, RTLD_LAZY);
-    FREE_TABLE(L_lib_name);
     if (L_library_handle == NULL) {
       GEN_ERROR(E_GEN_FATAL_ERROR, 
                 "Unable to open library file [" 
@@ -203,6 +202,7 @@ int C_ProtocolText::set_filter_method (char *P_filter_method) {
                 << "] error [" << dlerror() << "]");
       L_ret = -1 ;
     }
+    FREE_TABLE(L_lib_name);
   }
 
   L_fun_name = find_text_value(P_filter_method,(char*)"function") ;